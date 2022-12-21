fb28d6aad146ce1a7fea751043944bfa4b86326a
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-12-21 16:19:06 +0200

    m

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import {useState} from "react";
+import Statistics from "components/Statistics/Statistics";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Notification from "./Notification";
+import "./style.css";
+export default function App() {
+  const [good,setGood]= useState(0);
+  const [neutral,setNeutral]= useState(0);
+  const [bad,setBad]= useState(0);
+  const addFetback=(event)=>{
+        const{name}= event.currentTarget;
+        switch (name) {
+          case 'good':
+            setGood(good + 1)
+            break;
+          case 'neutral':
+            setNeutral(neutral + 1)
+            break;
+            case 'bad':
+              setBad(bad + 1)
+              break;
+          default:
+            break;
+        }
+      }
+      const countTotalFeedback =()=>{
+              return good + neutral + bad;
+            }
+      const countPositiveFeedbackPercentage =()=>{
+                  const positive = Math.round((good / countTotalFeedback()) * 100) 
+                  return positive;
+                  }
+      return  <> 
+          <h2>Please leave feedback</h2>
+          <FeedbackOptions  onLeaveFeedback={addFetback} mas={['good','neutral','bad']}/> 
+          <h2>Statistics</h2> 
+          {countTotalFeedback()?
+           <Statistics  good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()}/>
+           :<Notification message="There is no feedback"/>}
+          
+          </>
+          
+          
+        
+}
